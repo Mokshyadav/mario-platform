45b451c760bf9808a6024cafc6778c8f96267db0
Mokshyadav <83119079+Mokshyadav@users.noreply.github.com>
2021-05-08 16:45:14 +0530

    Add files via upload

---
+++ mario platform 1.js
@@ -0,0 +1,33 @@
+var countDistanceX = 0;
+var gap = 60
+
+function preload()
+{}
+
+function setup() {
+
+
+
+
+  createCanvas(9000, 668);
+
+ for(var i=0; i<10;i++){
+  platform1= new Platform(countDistanceX)
+  gap = random([0,20,40,50,80])
+  countDistanceX=countDistanceX+platform1.rw+gap
+
+
+  
+ }
+
+
+}
+
+function draw() {
+
+  background('skyblue'); 
+
+ drawSprites();
+
+}
+
